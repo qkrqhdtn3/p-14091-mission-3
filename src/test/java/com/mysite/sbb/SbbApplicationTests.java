c49f1ac8ceebf457e5b0f4570a1d777e2082deef
qkrqhdtn3 <qkrqhdtn3@gmail.com>
2025-11-29 21:55:08 +0900

    chore: move test files to new package

---
+++ src/test/java/com/mysite/sbb/SbbApplicationTests.java
@@ -0,0 +1,28 @@
+package com.mysite.sbb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.time.LocalDateTime;
+
+@SpringBootTest
+class SbbApplicationTests {
+//    @Autowired
+//    private QuestionRepository questionRepository;
+//
+//	@Test
+//	void testJpa() {
+//        Question question1 = new Question();
+//        question1.setSubject("sbb가 무엇인가요");
+//        question1.setContent("sbb에 대해서 알고 싶습니다");
+//        question1.setCreateDate(LocalDateTime.now());
+//        this.questionRepository.save(question1);
+//
+//        Question question2 = new Question();
+//        question2.setSubject("스프링부트 모델 질문");
+//        question2.setContent("id는 자동 생성 되나요?");
+//        question2.setCreateDate(LocalDateTime.now());
+//        this.questionRepository.save(question2);
+//	}
+}
