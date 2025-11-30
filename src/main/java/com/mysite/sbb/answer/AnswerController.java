aebf819b8c6a09298ca97c840104e2412dd725c0
qkrqhdtn3 <qkrqhdtn3@gmail.com>
2025-11-30 18:19:44 +0900

    feat: add answer creation logic in AnswerController

--- src/main/java/com/mysite/sbb/answer/AnswerController.java
+++ src/main/java/com/mysite/sbb/answer/AnswerController.java
@@ -12,13 +12,14 @@ import org.springframework.web.bind.annotation.*;
 @Controller
 public class AnswerController {
     private final QuestionService questionService;
+    private final AnswerService answerService;
 
     @PostMapping("/create/{id}")
     public String createAnswer(Model model,
                                @PathVariable Integer id,
                                @RequestParam(value="content") String content){
         Question question = this.questionService.getQuestion(id);
-//        TODO: 답변 저장
+        this.answerService.create(question, content);
         return String.format("redirect:/question/detail/%s", id);
     }
 }
