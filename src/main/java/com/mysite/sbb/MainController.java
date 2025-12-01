11e631c5bafb412666f562b12d49fec6358127ff
qkrqhdtn3 <qkrqhdtn3@gmail.com>
2025-12-01 09:29:13 +0900

    feat: add root redirect to /question/list

--- src/main/java/com/mysite/sbb/MainController.java
+++ src/main/java/com/mysite/sbb/MainController.java
@@ -5,9 +5,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
-@ResponseBody
 public class MainController {
     @GetMapping("/sbb")
+    @ResponseBody
     public String index(){
         return "안녕하세요 sbb에 오신 것을 환영합니다";
     }
